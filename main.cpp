32cad7c8d1665a0f8aec57cc55baec3ca692374f
KkGMIG29 <>
2022-01-24 11:20:28 +0000

    [Yellow] Templates Part1: Sqr all

--- main.cpp
+++ main.cpp
@@ -1,17 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int n, r;
-  cin >> n >> r;
-  
-  uint64_t total_weight = 0;
+template <typename T> vector<T> Sqr(const vector<T> &v);
+template <typename K, typename V> map<K, V> Sqr(const map<K, V> &mp);
+template <typename F, typename S> pair<F, S> Sqr(const pair<F, S> &p);
+template <typename T> T Sqr(const T& t);
 
-  for (int i = 0; i < n; i++) {
-    int w, h, d;
-    cin >> w >> h >> d;
-    total_weight += static_cast<uint64_t>(w) * h * d * r;
+template <typename T>
+vector<T> Sqr(const vector<T> &v) {
+  vector<T> res;
+  res.reserve(v.size());
+  for (const auto &x : v)
+    res.push_back(Sqr(x));
+  return res;
+}
+
+template <typename K, typename V>
+map<K, V> Sqr(const map<K, V> &mp) {
+  map<K, V> res;
+  for (const auto &[k, v] : mp)
+    res[k] = Sqr(v);
+  return res;
+}
+
+template <typename F, typename S>
+pair<F, S> Sqr(const pair<F, S> &p) {
+  return {Sqr(p.first), Sqr(p.second)};
+}
+
+template <typename T> T Sqr(const T& t) {
+  return t * t;
+}
+
+int main() {
+  map<int, pair<int, int>> map_of_pairs = {
+  {4, {2, 2}},
+  {7, {4, 3}}
+  };
+  cout << "map of pairs:" << endl;
+  for (const auto& x : Sqr(map_of_pairs)) {
+    cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
   }
 
-  cout << total_weight;
 } 
