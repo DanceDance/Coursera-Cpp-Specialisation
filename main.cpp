1429cdefec63e6c5ff1d34fba5e0f609c717fbb6
KkGMIG29 <>
2022-02-18 01:45:03 +0000

    [Yellow] PrevPermutation

--- main.cpp
+++ main.cpp
@@ -3,28 +3,21 @@
 #include <set>
 #include <vector>
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
-template <typename T>
-void RemoveDuplicates(vector<T>& elements) {
-  sort(begin(elements), end(elements));
-  elements.erase(unique(begin(elements), end(elements)), end(elements));
-}
-
 int main() {
-  vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
-  RemoveDuplicates(v1);
-  for (int x : v1) {
-    cout << x << " ";
-  }
-  cout << endl;
-  
-  vector<string> v2 = {"C", "C++", "C++", "C", "C++"};
-  RemoveDuplicates(v2);
-  for (const string& s : v2) {
-    cout << s << " ";
-  }
-  cout << endl;
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  iota(begin(a), end(a), 1);
+  reverse(begin(a), end(a));
+  do {
+    for (const int &x : a)
+      cout << x << " ";
+    cout << "\n";
+  } while (prev_permutation(begin(a), end(a)));
+
   return 0;
 }
