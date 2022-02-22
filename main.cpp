1e06b74e09bccb097f4cf4133c34cdbf0f1e080d
KkGMIG29 <>
2022-02-22 09:35:33 +0000

    [Yellow] build expression hard

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 #include <iostream>
 #include <numeric>
 #include <iterator>
+#include <sstream>
+#include <map>
 
 using namespace std;
 
 int main() {
+  static map<string, int> priority {
+    {"+", 1},
+    {"-", 1},
+    {"*", 2},
+    {"/", 2}  
+  };
   string first_var;
   cin >> first_var;
   int n;
@@ -20,10 +28,18 @@ int main() {
     cin >> ops[i];
     cin >> vars[i];    
   }
-  cout << string(n, '(');
-  cout << first_var;
+  int parentheses_count = 0;
+  stringstream right;
+  right << first_var;
+
   for (int i = 0; i < n; i++) {
-    cout << ')' << ' ' << ops[i] << ' ' << vars[i];
+    if (i > 0 && priority[ops[i - 1]] < priority[ops[i]]) {
+      right << ')';
+      parentheses_count++;
+    }
+    right << ' ' << ops[i] << ' ' << vars[i];
   }
+  cout << string(parentheses_count, '(');
+  cout << right.str();
   return 0;
 }
