e6c393f2a513ee43005f8b0a7e9ab657698bf71a
KkGMIG29 <>
2022-02-22 02:23:02 +0000

    [Yellow] find_starts_with_str

--- main.cpp
+++ main.cpp
@@ -11,32 +11,33 @@ using namespace std;
 template <typename RandomIt>
 pair<RandomIt, RandomIt> FindStartsWith(
     RandomIt range_begin, RandomIt range_end,
-    char prefix) {
-  string prefix_s = string(1, prefix);
-  string prefix_s_next = string(1, prefix + 1);
-  cerr << prefix_s << " " << prefix_s_next << "\n";
-  return {lower_bound(range_begin, range_end, prefix_s), lower_bound(range_begin, range_end, prefix_s_next)};
+    const string& prefix) {
+  string next_prefix = prefix;
+  char last_char = *rbegin(next_prefix);
+  last_char++;
+  *rbegin(next_prefix) = last_char;
+  return {lower_bound(range_begin, range_end, prefix), lower_bound(range_begin, range_end, next_prefix)};
 }
 
 int main() {
-  const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+  const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
   
-  const auto m_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
-  for (auto it = m_result.first; it != m_result.second; ++it) {
+  const auto mo_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+  for (auto it = mo_result.first; it != mo_result.second; ++it) {
     cout << *it << " ";
   }
   cout << endl;
   
-  const auto p_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
-  cout << (p_result.first - begin(sorted_strings)) << " " <<
-      (p_result.second - begin(sorted_strings)) << endl;
+  const auto mt_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+  cout << (mt_result.first - begin(sorted_strings)) << " " <<
+      (mt_result.second - begin(sorted_strings)) << endl;
   
-  const auto z_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
-  cout << (z_result.first - begin(sorted_strings)) << " " <<
-      (z_result.second - begin(sorted_strings)) << endl;
+  const auto na_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+  cout << (na_result.first - begin(sorted_strings)) << " " <<
+      (na_result.second - begin(sorted_strings)) << endl;
   
   return 0;
 }
