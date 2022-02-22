911cd0f0056eff06983c6f54169a79b77b6f5d44
KkGMIG29 <>
2022-02-22 07:52:41 +0000

    [Yellow] build expression simple

--- main.cpp
+++ main.cpp
@@ -8,36 +8,22 @@
 
 using namespace std;
 
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(
-    RandomIt range_begin, RandomIt range_end,
-    const string& prefix) {
-  string next_prefix = prefix;
-  char last_char = *rbegin(next_prefix);
-  last_char++;
-  *rbegin(next_prefix) = last_char;
-  return {lower_bound(range_begin, range_end, prefix), lower_bound(range_begin, range_end, next_prefix)};
-}
-
 int main() {
-  const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
-  
-  const auto mo_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-  for (auto it = mo_result.first; it != mo_result.second; ++it) {
-    cout << *it << " ";
+  string first_var;
+  cin >> first_var;
+  int n;
+  cin >> n;
+  vector<string> vars(n);
+  vector<string> ops(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> ops[i];
+    cin >> vars[i];    
   }
-  cout << endl;
-  
-  const auto mt_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-  cout << (mt_result.first - begin(sorted_strings)) << " " <<
-      (mt_result.second - begin(sorted_strings)) << endl;
-  
-  const auto na_result =
-      FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-  cout << (na_result.first - begin(sorted_strings)) << " " <<
-      (na_result.second - begin(sorted_strings)) << endl;
-  
+  cout << string(n, '(');
+  cout << first_var;
+  for (int i = 0; i < n; i++) {
+    cout << ')' << ' ' << ops[i] << ' ' << vars[i];
+  }
   return 0;
 }
