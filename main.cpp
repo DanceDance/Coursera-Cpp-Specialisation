6d3d027d8183d587fe11d42f3f99103c239d2e4d
KkGMIG29 <>
2022-03-03 08:23:02 +0000

    [Red] PRINT_VALUES

--- main.cpp
+++ main.cpp
@@ -1,96 +1,20 @@
-#include "airline_ticket.h"
 #include "test_runner.h"
 
-#include <iostream>
-#include <iomanip>
-
+#include <ostream>
+#include <string>
 using namespace std;
 
-#define UPDATE_FIELD(ticket, field, values) \
-{ \
-  auto it = (values).find(#field); \
-  if (it != (values).end()) { \
-    istringstream is(it->second); \
-    is >> (ticket).field; \
-  } \
-}
-
-bool operator< (const Date& lhs, const Date& rhs) {
-  return tie(lhs.year, lhs.month, lhs.day) < tie(rhs.year, rhs.month, rhs.day);
-}
-
-bool operator== (const Date& lhs, const Date& rhs) {
-  return tie(lhs.year, lhs.month, lhs.day) == tie(rhs.year, rhs.month, rhs.day);
-}
-
-bool operator< (const Time& lhs, const Time& rhs) {
-  return tie(lhs.hours, lhs.minutes) < tie(rhs.hours, rhs.minutes);
-}
-
-bool operator== (const Time& lhs, const Time& rhs) {
-  return tie(lhs.hours, lhs.minutes) == tie(rhs.hours, rhs.minutes);
-}
-
-ostream& operator<< (ostream& os, const Date& date) {
-  os << date.year << "-" << date.month << "-" << date.day;
-  return os;
-}
-
-ostream& operator<< (ostream& os, const Time& time) {
-  os << setfill('0') << setw(2) << time.hours << ":" << setfill('0') << setw(2) << time.minutes;
-  return os;
-}
-
-istream& operator>> (istream& is, Date& date) {
-  is >> date.year;
-  is.ignore(1);
-  is >> date.month;
-  is.ignore(1);
-  is >> date.day;
-  return is;
-}
-
-istream& operator>>  (istream& is, Time& time) {
-  is >> time.hours;
-  is.ignore(1);
-  is >> time.minutes;
-  return is;
-}
-
-void TestUpdate() {
-  AirlineTicket t;
-  t.price = 0;
-
-  const map<string, string> updates1 = {
-    {"departure_date", "2018-2-28"},
-    {"departure_time", "17:40"},
-  };
-  UPDATE_FIELD(t, departure_date, updates1);
-  UPDATE_FIELD(t, departure_time, updates1);
-  UPDATE_FIELD(t, price, updates1);
-
-  ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
-  ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
-  ASSERT_EQUAL(t.price, 0);
-
-  const map<string, string> updates2 = {
-    {"price", "12550"},
-    {"arrival_time", "20:33"},
-  };
-  UPDATE_FIELD(t, departure_date, updates2);
-  UPDATE_FIELD(t, departure_time, updates2);
-  UPDATE_FIELD(t, arrival_time, updates2);
-  UPDATE_FIELD(t, price, updates2);
-
-  // updates2 не содержит ключей "departure_date" и "departure_time", поэтому
-  // значения этих полей не должны измениться
-  ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
-  ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
-  ASSERT_EQUAL(t.price, 12550);
-  ASSERT_EQUAL(t.arrival_time, (Time{20, 33}));
-}
+#define PRINT_VALUES(out, x, y) (out) << (x) << endl << (y) << endl
 
 int main() {
   TestRunner tr;
-  RUN_TEST(tr, TestUpdate);
+  tr.RunTest([] {
+    ostringstream output;
+    PRINT_VALUES(output, 5, "red belt");
+    ASSERT_EQUAL(output.str(), "5\nred belt\n");
+    output = ostringstream();
+    PRINT_VALUES(output, 25 + 5, string("red ") + "belt");
+    ASSERT_EQUAL(output.str(), "30\nred belt\n");
+  }, "PRINT_VALUES usage example");
+  return 0;
 }
