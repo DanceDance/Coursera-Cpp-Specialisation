08a09655133387f59848d96e03200779e4025d6c
KkGMIG29 <>
2022-03-03 00:55:14 +0000

    [Red] airline_ticket

--- main.cpp
+++ main.cpp
@@ -1,103 +1,63 @@
-#include "database.h"
-#include "date.h"
-#include "condition_parser.h"
-#include "node.h"
+#include "airline_ticket.h"
 #include "test_runner.h"
-#include "util.h"
 
-#include <iostream>
-#include <string>
-#include <stdexcept>
-#include <memory>
-
+#include <tuple> 
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
 using namespace std;
 
-void TestAll();
+#define SORT_BY(field) [](const AirlineTicket& lhs, const AirlineTicket& rhs) { return lhs.field < rhs.field; }
+
+bool operator< (const Date& lhs, const Date& rhs) {
+  return tie(lhs.year, lhs.month, lhs.day) < tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator== (const Date& lhs, const Date& rhs) {
+  return tie(lhs.year, lhs.month, lhs.day) == tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator< (const Time& lhs, const Time& rhs) {
+  return tie(lhs.hours, lhs.minutes) < tie(rhs.hours, rhs.minutes);
+}
+
+bool operator== (const Time& lhs, const Time& rhs) {
+  return tie(lhs.hours, lhs.minutes) == tie(rhs.hours, rhs.minutes);
+}
+
+ostream& operator<< (ostream& os, const Date& date) {
+  os << date.year << "-" << date.month << "-" << date.day;
+  return os;
+}
+
+ostream& operator<< (ostream& os, const Time& time) {
+  os << setfill('0') << setw(2) << time.hours << ":" << setfill('0') << setw(2) << time.minutes;
+  return os;
+}
+
+void TestSortBy() {
+  vector<AirlineTicket> tixs = {
+    {"VKO", "AER", "Utair",     {2018, 2, 28}, {17, 40}, {2018, 2, 28}, {20,  0}, 1200},
+    {"AER", "VKO", "Utair",     {2018, 3,  5}, {14, 15}, {2018, 3,  5}, {16, 30}, 1700},
+    {"AER", "SVO", "Aeroflot",  {2018, 3,  5}, {18, 30}, {2018, 3,  5}, {20, 30}, 2300},
+    {"PMI", "DME", "Iberia",    {2018, 2,  8}, {23, 00}, {2018, 2,  9}, { 3, 30}, 9000},
+    {"CDG", "SVO", "AirFrance", {2018, 3,  1}, {13, 00}, {2018, 3,  1}, {17, 30}, 8000},
+  };
+
+  sort(begin(tixs), end(tixs), SORT_BY(price));
+  ASSERT_EQUAL(tixs.front().price, 1200);
+  ASSERT_EQUAL(tixs.back().price, 9000);
+
+  sort(begin(tixs), end(tixs), SORT_BY(from));
+  ASSERT_EQUAL(tixs.front().from, "AER");
+  ASSERT_EQUAL(tixs.back().from, "VKO");
+
+  sort(begin(tixs), end(tixs), SORT_BY(arrival_date));
+  ASSERT_EQUAL(tixs.front().arrival_date, (Date{2018, 2, 9}));
+  ASSERT_EQUAL(tixs.back().arrival_date, (Date{2018, 3, 5}));
+}
 
 int main() {
-  TestAll();
-
-  Database db;
-
-  for (string line; getline(cin, line); ) {
-    istringstream is(line);
-
-    string command;
-    is >> command;
-    if (command == "Add") {
-      const auto date = ParseDate(is);
-      const auto event = ParseEvent(is);
-      db.Add(date, event);
-    } else if (command == "Print") {
-      db.Print(cout);
-    } else if (command == "Del") {
-      auto condition = ParseCondition(is);
-      auto predicate = [condition](const Date& date, const string& event) {
-        return condition->Evaluate(date, event);
-      };
-      int count = db.RemoveIf(predicate);
-      cout << "Removed " << count << " entries" << endl;
-    } else if (command == "Find") {
-      auto condition = ParseCondition(is);
-      auto predicate = [condition](const Date& date, const string& event) {
-        return condition->Evaluate(date, event);
-      };
-
-      const auto entries = db.FindIf(predicate);
-      for (const auto& entry : entries) {
-        cout << entry << endl;
-      }
-      cout << "Found " << entries.size() << " entries" << endl;
-    } else if (command == "Last") {
-      try {
-          cout << db.Last(ParseDate(is)) << endl;
-      } catch (invalid_argument&) {
-          cout << "No entries" << endl;
-      }
-    } else if (command.empty()) {
-      continue;
-    } else {
-      throw logic_error("Unknown command: " + command);
-    }
-  }
-
-  return 0;
-}
-
-void TestParseEvent() {
-  {
-    istringstream is("event");
-    AssertEqual(ParseEvent(is), "event", "Parse event without leading spaces");
-  }
-  {
-    istringstream is("   sport event ");
-    AssertEqual(ParseEvent(is), "sport event ", "Parse event with leading spaces");
-  }
-  {
-    istringstream is("  first event  \n  second event");
-    vector<string> events;
-    events.push_back(ParseEvent(is));
-    events.push_back(ParseEvent(is));
-    AssertEqual(events, vector<string>{"first event  ", "second event"}, "Parse multiple events");
-  }
-}
-
-void TestDatabase() {
-  Database db;
-  for (int i = 0; i < 1e6; i++) {
-    db.Add({rand() % 9999, rand() % 12 + 1, rand() % 31 + 1}, "event" + to_string(i));
-  }
-  auto res = db.FindIf([] (const Date &d, const string &s) {return true;});
-  cerr << res.size() << "\n";
-  int removed = db.RemoveIf([] (const Date &d, const string &s) {return true;});
-  cerr << removed << "\n";
-}
-
-void TestAll() {
   TestRunner tr;
-  tr.RunTest(TestParseEvent, "TestParseEvent");
-  tr.RunTest(TestParseCondition, "TestParseCondition");
-  //tr.RunTest(TestDatabase, "TestSpeed");
+  RUN_TEST(tr, TestSortBy);
 }
-
-
