f85294fa5ef8cf9e855af52e0438f58d131ca4b8
KkGMIG29 <>
2022-03-08 05:20:35 +0000

    [Red] Student optimiziation

--- main.cpp
+++ main.cpp
@@ -1,146 +1,63 @@
+#include "student.h"
 #include "test_runner.h"
 
-#include <numeric>
-#include <iostream>
-#include <vector>
-#include <string>
+#include <algorithm>
+
 using namespace std;
 
-// Реализуйте шаблон класса Paginator
-
-template <typename Iterator>
-class IteratorRange {
-private:
-  Iterator begin_;
-  Iterator end_;
-public:
-  IteratorRange(Iterator begin, Iterator end) : begin_(begin), end_(end) {}
-  Iterator begin() { return begin_; }
-  Iterator end() { return end_; }
-  Iterator begin() const { return begin_; }
-  Iterator end() const { return end_; }
-  size_t size() const { return end_ - begin_; }
-};
-
-template <typename Iterator>
-class Paginator {
-private:
-  vector<IteratorRange<Iterator>> v; 
-public:
-  Paginator(Iterator begin, Iterator end, size_t page_size) {
-    size_t n = end - begin;
-    for (size_t i = 0; i < n; i += page_size) {
-      size_t cur_page_size = min(page_size, n - i);
-      v.push_back(IteratorRange(begin + i, begin + i + cur_page_size));
-    }
-  }
-  auto begin() { return v.begin(); }
-  auto end() { return v.end(); }
-  size_t size() const { return v.size(); }
-};
-
-template <typename C>
-auto Paginate(C& c, size_t page_size) {
-  return Paginator(begin(c), end(c), page_size);
+//Оптимизируйте эту функцию
+bool Compare(const Student &first, const Student &second) {
+  return first.Less(second);
 }
 
-void TestPageCounts() {
-  vector<int> v(15);
-
-  ASSERT_EQUAL(Paginate(v, 1).size(), v.size());
-  ASSERT_EQUAL(Paginate(v, 3).size(), 5u);
-  ASSERT_EQUAL(Paginate(v, 5).size(), 3u);
-  ASSERT_EQUAL(Paginate(v, 4).size(), 4u);
-  ASSERT_EQUAL(Paginate(v, 15).size(), 1u);
-  ASSERT_EQUAL(Paginate(v, 150).size(), 1u);
-  ASSERT_EQUAL(Paginate(v, 14).size(), 2u);
-}
-
-void TestLooping() {
-  vector<int> v(15);
-  iota(begin(v), end(v), 1);
-
-  Paginator<vector<int>::iterator> paginate_v(v.begin(), v.end(), 6);
-  ostringstream os;
-  for (const auto& page : paginate_v) {
-    for (int x : page) {
-      os << x << ' ';
-    }
-    os << '\n';
-  }
-  ASSERT_EQUAL(os.str(), "1 2 3 4 5 6 \n7 8 9 10 11 12 \n13 14 15 \n");
-}
-
-void TestModification() {
-  vector<string> vs = {"one", "two", "three", "four", "five"};
-  for (auto page : Paginate(vs, 2)) {
-    for (auto& word : page) {
-      word[0] = toupper(word[0]);
-    }
-  }
-
-  const vector<string> expected = {"One", "Two", "Three", "Four", "Five"};
-  ASSERT_EQUAL(vs, expected);
-}
-
-void TestPageSizes() {
-  string letters(26, ' ');
-
-  Paginator letters_pagination(letters.begin(), letters.end(), 11);
-  vector<size_t> page_sizes;
-  for (const auto& page : letters_pagination) {
-    page_sizes.push_back(page.size());
-  }
-
-  const vector<size_t> expected = {11, 11, 4};
-  ASSERT_EQUAL(page_sizes, expected);
-}
-
-void TestConstContainer() {
-  const string letters = "abcdefghijklmnopqrstuvwxyz";
-
-  vector<string> pages;
-  for (const auto& page : Paginate(letters, 10)) {
-    pages.push_back(string(page.begin(), page.end()));
-  }
-
-  const vector<string> expected = {"abcdefghij", "klmnopqrst", "uvwxyz"};
-  ASSERT_EQUAL(pages, expected);
-}
-
-void TestPagePagination() {
-  vector<int> v(22);
-  iota(begin(v), end(v), 1);
-
-  vector<vector<int>> lines;
-  for (const auto& split_by_9 : Paginate(v, 9)) {
-    for (const auto& split_by_4 : Paginate(split_by_9, 4)) {
-      lines.push_back({});
-      for (int item : split_by_4) {
-        lines.back().push_back(item);
-      }
-    }
-  }
-
-  const vector<vector<int>> expected = {
-      {1, 2, 3, 4},
-      {5, 6, 7, 8},
-      {9},
-      {10, 11, 12, 13},
-      {14, 15, 16, 17},
-      {18},
-      {19, 20, 21, 22}
+void TestComparison() {
+  Student newbie {
+    "Ivan", "Ivanov", {
+      {"c++", 1.0},
+      {"algorithms", 3.0}
+    },
+    2.0
   };
-  ASSERT_EQUAL(lines, expected);
+
+  Student cpp_expert {
+    "Petr", "Petrov", {
+      {"c++", 9.5},
+      {"algorithms", 6.0}
+    },
+    7.75
+  };
+
+  Student guru {
+    "Sidor", "Sidorov", {
+      {"c++", 10.0},
+      {"algorithms", 10.0}
+    },
+    10.0
+  };
+  ASSERT(Compare(guru, newbie));
+  ASSERT(Compare(guru, cpp_expert));
+  ASSERT(!Compare(newbie, cpp_expert));
+}
+
+void TestSorting() {
+  vector<Student> students {
+    {"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+    {"Semen", "Semenov", {{"maths", 4.}}, 4.},
+    {"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+    {"Petr", "Petrov", {{"maths", 3.}}, 3.},
+    {"Alexander", "Alexandrov", {{"maths", 1.}}, 1.}
+  };
+  sort(students.begin(), students.end(), Compare);
+  ASSERT(is_sorted(students.begin(), students.end(),
+    [](Student first, Student second) {
+      return first.Less(second);
+    })
+  );
 }
 
 int main() {
   TestRunner tr;
-  RUN_TEST(tr, TestPageCounts);
-  RUN_TEST(tr, TestLooping);
-  RUN_TEST(tr, TestModification);
-  RUN_TEST(tr, TestPageSizes);
-  RUN_TEST(tr, TestConstContainer);
-  RUN_TEST(tr, TestPagePagination);
+  RUN_TEST(tr, TestComparison);
+  RUN_TEST(tr, TestSorting);
+  return 0;
 }
-
