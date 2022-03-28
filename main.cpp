a494687843b776d27fd6996ff433dcf6072829c6
KkGMIG29 <>
2022-03-28 09:45:19 +0000

    [Red] NumbersOrder

--- main.cpp
+++ main.cpp
@@ -1,143 +1,28 @@
 #include "test_runner.h"
 
+#include <list>
+#include <map>
 #include <vector>
+#include <iostream>
 using namespace std;
 
-template <typename T>
-class LinkedList {
-public:
-  struct Node {
-    Node() = default;
-    explicit Node(const T& value) : value(value) {}
-    T value;
-    Node* next = nullptr;
-  };
-
-  ~LinkedList() {
-    while (head != nullptr)
-      PopFront();
-  }
-
-  void PushFront(const T& value) {
-    Node* new_node = new Node(value);
-    new_node->next = head;
-    head = new_node;
-  }
-
-  void InsertAfter(Node* node, const T& value) {
-    if (node == nullptr)
-      PushFront(value);
-    else {
-      Node* new_node = new Node(value);
-      new_node->next = node->next;
-      node->next = new_node;
-    }
-  }
-
-  void RemoveAfter(Node* node) {
-    if (node == nullptr)
-      PopFront();
-    else if (node->next != nullptr) {
-      Node* next_next = node->next->next;
-      delete node->next;
-      node->next = next_next;
-    }
-  }
-
-  void PopFront() {
-    if (head != nullptr) {
-      Node* next_node = head->next;
-      delete head;
-      head = next_node;      
-    }
-  }
-
-  Node* GetHead() { return head; }
-  const Node* GetHead() const { return head; }
-
-private:
-  Node* head = nullptr;
-};
-
-template <typename T>
-vector<T> ToVector(const LinkedList<T>& list) {
-  vector<T> result;
-  for (auto node = list.GetHead(); node; node = node->next) {
-    result.push_back(node->value);
-  }
-  return result;
-}
-
-void TestPushFront() {
-  LinkedList<int> list;
-
-  list.PushFront(1);
-  ASSERT_EQUAL(list.GetHead()->value, 1);
-  list.PushFront(2);
-  ASSERT_EQUAL(list.GetHead()->value, 2);
-  list.PushFront(3);
-  ASSERT_EQUAL(list.GetHead()->value, 3);
-
-  const vector<int> expected = {3, 2, 1};
-  ASSERT_EQUAL(ToVector(list), expected);
-}
-
-void TestInsertAfter() {
-  LinkedList<string> list;
-
-  list.PushFront("a");
-  auto head = list.GetHead();
-  ASSERT(head);
-  ASSERT_EQUAL(head->value, "a");
-
-  list.InsertAfter(head, "b");
-  const vector<string> expected1 = {"a", "b"};
-  ASSERT_EQUAL(ToVector(list), expected1);
-
-  list.InsertAfter(head, "c");
-  const vector<string> expected2 = {"a", "c", "b"};
-  ASSERT_EQUAL(ToVector(list), expected2);
-}
-
-void TestRemoveAfter() {
-  LinkedList<int> list;
-  for (int i = 1; i <= 5; ++i) {
-    list.PushFront(i);
-  }
-
-  const vector<int> expected = {5, 4, 3, 2, 1};
-  ASSERT_EQUAL(ToVector(list), expected);
-
-  auto next_to_head = list.GetHead()->next;
-  list.RemoveAfter(next_to_head); // удаляем 3
-  list.RemoveAfter(next_to_head); // удаляем 2
-
-  const vector<int> expected1 = {5, 4, 1};
-  ASSERT_EQUAL(ToVector(list), expected1);
-
-  while (list.GetHead()->next) {
-    list.RemoveAfter(list.GetHead());
-  }
-  ASSERT_EQUAL(list.GetHead()->value, 5);
-}
-
-void TestPopFront() {
-  LinkedList<int> list;
-
-  for (int i = 1; i <= 5; ++i) {
-    list.PushFront(i);
-  }
-  for (int i = 1; i <= 5; ++i) {
-    list.PopFront();
-  }
-  ASSERT(list.GetHead() == nullptr);
-}
-
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestPushFront);
-  RUN_TEST(tr, TestInsertAfter);
-  RUN_TEST(tr, TestRemoveAfter);
-  RUN_TEST(tr, TestPopFront);
+  list<int> l;
+  map<int, list<int>::iterator> mp;
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int num, before_num;
+    cin >> num >> before_num;
+    auto it = l.end();
+    auto mp_it = mp.find(before_num);
+    if (mp_it != mp.end()) {
+      it = mp_it->second;
+    }
+    mp[num] = l.insert(it, num);
+  }
+  for (auto &x : l)
+    cout << x << "\n";
   return 0;
 }
