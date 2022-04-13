2711805b60c519f769a1df5f2f9428f8ce4b5e12
KkGMIG29 <>
2022-04-13 11:53:11 +0000

    [Red] MergeSort no copy

--- main.cpp
+++ main.cpp
@@ -1,38 +1,24 @@
-#include "simple_vector_2.h"
 #include "test_runner.h"
-
 #include <algorithm>
-#include <iostream>
+#include <memory>
 #include <vector>
+#include <iterator>
 #include <string>
+
 using namespace std;
 
-void TestConstruction() {
-  SimpleVector<int> empty;
-  ASSERT_EQUAL(empty.Size(), 0u);
-  ASSERT_EQUAL(empty.Capacity(), 0u);
-  ASSERT(empty.begin() == empty.end());
-
-  SimpleVector<string> five_strings(5);
-  ASSERT_EQUAL(five_strings.Size(), 5u);
-  ASSERT(five_strings.Size() <= five_strings.Capacity());
-  for (auto& item : five_strings) {
-    ASSERT(item.empty());
-  }
-  five_strings[2] = "Hello";
-  ASSERT_EQUAL(five_strings[2], "Hello");
-}
-
-void TestPushBack() {
-  SimpleVector<int> v;
-  for (int i = 10; i >= 1; --i) {
-    v.PushBack(i);
-    ASSERT(v.Size() <= v.Capacity());
-  }
-  sort(begin(v), end(v));
-
-  const vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  ASSERT(equal(begin(v), end(v), begin(expected)));
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+  int n = range_end - range_begin;
+  if (n < 2)
+    return;
+  vector<typename RandomIt::value_type> tmp(make_move_iterator(range_begin), make_move_iterator(range_end));
+  MergeSort(begin(tmp), begin(tmp) + n/3);
+  MergeSort(begin(tmp) + n/3, begin(tmp) + 2 * n / 3);
+  MergeSort(begin(tmp) + 2 * n / 3, end(tmp));
+  vector<typename RandomIt::value_type> tmp2;
+  merge(make_move_iterator(begin(tmp)), make_move_iterator(begin(tmp)) + n/3, make_move_iterator(begin(tmp)) + n/3, make_move_iterator(begin(tmp)) + 2 * n / 3, back_inserter(tmp2));
+  merge(make_move_iterator(begin(tmp2)), make_move_iterator(end(tmp2)), make_move_iterator(begin(tmp)) + 2 * n / 3, make_move_iterator(end(tmp)), range_begin);
 }
 
 class StringNonCopyable : public string {
@@ -46,20 +32,21 @@ public:
 };
 
 void TestNoCopy() {
-  SimpleVector<StringNonCopyable> strings;
-  static const int SIZE = 10;
-  for (int i = 0; i < SIZE; ++i) {
-    strings.PushBack(StringNonCopyable(to_string(i)));
-  }
-  for (int i = 0; i < SIZE; ++i) {
-    ASSERT_EQUAL(strings[i], to_string(i));
-  }
+  vector<string> strings_original = {"b", "c", "a"};
+  vector<StringNonCopyable> strings(make_move_iterator(begin(strings_original)), make_move_iterator(end(strings_original)));
+  MergeSort(begin(strings), end(strings));
+  ASSERT(is_sorted(begin(strings), end(strings)));
+}
+
+void TestIntVector() {
+  vector<int> numbers = {6, 1, 3, 9, 1, 9, 8, 12, 1};
+  MergeSort(begin(numbers), end(numbers));
+  ASSERT(is_sorted(begin(numbers), end(numbers)));
 }
 
 int main() {
   TestRunner tr;
-  RUN_TEST(tr, TestConstruction);
-  RUN_TEST(tr, TestPushBack);
+  RUN_TEST(tr, TestIntVector);
   RUN_TEST(tr, TestNoCopy);
   return 0;
 }
